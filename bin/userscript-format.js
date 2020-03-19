1e7a6eed31a6a97468e395ccb4d7b93e27d879fc
bitWolfy <bondarchuk.kirill@gmail.com>
2020-03-19 16:33:36 -0700

    Implement automatic build numbering

--- bin/userscript-format.js
+++ bin/userscript-format.js
@@ -3,10 +3,18 @@ const fs = require("fs");
 const template = fs.readFileSync("./bin/userscript-template.txt").toString();
 const json = JSON.parse(fs.readFileSync("./package.json"));
 
-const templateReplaced = template.replace(/%NAME%/g, json.displayName).
+var templateReplaced = template.replace(/%NAME%/g, json.displayName).
 replace(/%NAMESPACE%/g, json.namespace).
-replace(/%VERSION%/g, json.version).
 replace(/%DESCRIPTION%/g, json.description).
 replace(/%AUTHOR%/g, json.author);
+
+if (process.argv[2] === undefined) {
+    templateReplaced = templateReplaced.replace(/%VERSION%/g, json.version);
+} else {
+    templateReplaced = templateReplaced.replace(/%VERSION%/g,
+        json.version.substring(0, json.version.lastIndexOf(".")) +
+        process.argv[2].substring(process.argv[2].lastIndexOf("."))
+    );
+}
 
 fs.writeFileSync("./build/script.user.js", templateReplaced + fs.readFileSync("./build/script.user.js"));
