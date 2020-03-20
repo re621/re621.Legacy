9c9c203752884ba2f3bce4455b7e9b2eb3512d72
Earlopain <earlopain@c5h8no4na.net>
2020-03-20 19:49:09 +0100

    Added InstantSearch functionality Fixes #19

--- src/js/main.ts
+++ src/js/main.ts
@@ -18,11 +18,13 @@ import { BlacklistToggler } from "./modules/BlacklistToggler";
 import { FormattingHelper } from "./modules/FormattingHelper";
 import { TitleCustomizer } from "./modules/TitleCustomizer";
 import { MiscFunctionality } from "./modules/MiscFunctionality";
+import { InstantSearch } from "./modules/InstantSearch";
 
 HeaderCustomizer.getInstance();
 ThemeCustomizer.getInstance();
 BlacklistToggler.getInstance();
 TitleCustomizer.getInstance();
 MiscFunctionality.getInstance();
+InstantSearch.getInstance();
 
 FormattingHelper.init();
---
+++ src/js/modules/InstantSearch.ts
@@ -0,0 +1,61 @@
+import { RE6Module } from "../components/RE6Module";
+import { Url } from "../components/Url";
+import { Post } from "../components/Post";
+
+/**
+ * Adds a extra search input below the current one where 
+ * you can filter posts instantaneously
+ */
+export class InstantSearch extends RE6Module {
+
+    private locationConstrain = "=/posts|/posts?";
+
+    // TODO: this can be of type HTMLInputElememnt, but I don't know how to do that
+    private $searchbox: JQuery<HTMLElement>;
+
+    private static instance: InstantSearch = new InstantSearch();
+
+    private constructor() {
+        super();
+        // TODO: Maybe somehow put this into RE6Module
+        if (!Url.matches(this.locationConstrain)) {
+            return;
+        }
+
+        this.createDOM();
+
+        this.$searchbox.on("input", e => {
+            const filter = this.$searchbox.val().toString();
+            const posts = Post.getVisiblePosts();
+            if (filter === "") {
+                for (const post of posts) {
+                    $(post.getDomElement).css("display", "");
+                }
+            } else {
+                for (const post of posts) {
+                    if (post.tagsMatchesFilter(filter)) {
+                        post.getDomElement().css("display", "");
+                    } else {
+                        post.getDomElement().css("display", "none");
+                    }
+                }
+            }
+
+        })
+    }
+
+    protected createDOM() {
+        this.$searchbox = $("<input>").
+            attr("id", "re-instantsearch-input");
+        this.$searchbox.insertAfter($("#search-box"));
+    }
+
+    /**
+     * Returns a singleton instance of the class
+     * @returns FormattingHelper instance
+     */
+    public static getInstance() {
+        if (this.instance == undefined) this.instance = new InstantSearch();
+        return this.instance;
+    }
+}
