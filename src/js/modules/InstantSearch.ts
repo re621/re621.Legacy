bb3e8ab092476eabddf24af26e21b7f36bebd4cc
Earlopain <earlopain@c5h8no4na.net>
2020-03-21 16:13:06 +0100

    Preserve InstantSearch content when paginating Fixes #31

--- src/js/modules/InstantSearch.ts
+++ src/js/modules/InstantSearch.ts
@@ -26,9 +26,11 @@ export class InstantSearch extends RE6Module {
 
         this.$searchbox.on("input", () => {
             const filter = this.$searchbox.val().toString();
+            sessionStorage.setItem("instantsearch", filter);
             //TODO: It's probably excessive to get these every time, but
             //      let's keep it this way for now until something happens with the infinite scrolling mode
             const posts = Post.getVisiblePosts();
+            //when the user clears the input, show all posts
             if (filter === "") {
                 for (const post of posts) {
                     $(post.getDomElement()).css("display", "");
@@ -49,7 +51,8 @@ export class InstantSearch extends RE6Module {
     protected createDOM() {
         const $section = $("<section>").attr("id", "re-instantsearch");
         this.$searchbox = $("<input>").
-            attr("id", "re-instantsearch-input");
+            attr("id", "re-instantsearch-input").
+            attr("value", sessionStorage.getItem("instantsearch"));
         this.$searchbox.attr("type", "text");
         $section.append("<h1>Insant Search</h1>")
         $section.append(this.$searchbox)
