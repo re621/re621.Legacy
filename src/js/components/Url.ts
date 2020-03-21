71fc5f4f3bb63274ff1d1235a60f678f41c8ba50
Earlopain <earlopain@c5h8no4na.net>
2020-03-21 18:53:23 +0100

    Added query string helper function to url

--- src/js/components/Url.ts
+++ src/js/components/Url.ts
@@ -1,5 +1,28 @@
 export class Url {
 
+    private static instance: Url;
+
+    private location: Location;
+    private queryParameter: Map<string, string> = new Map<string, string>();
+
+    private constructor() {
+        this.location = document.location;
+        this.initQueryParams();
+    }
+
+    private initQueryParams() {
+        const paramString = this.location.href.split("?")[1];
+        //Check if there are any query params
+        if (paramString === undefined) {
+            return;
+        }
+        //Add each paramter to the map
+        for (const param of paramString.split("&")) {
+            const split = param.split("=")
+            this.queryParameter.set(split[0], split[1] !== undefined ? split[1] : "");
+        }
+    }
+
     /**
      * Checks if the url the user is currently on satisfies the filter
      * @param filter Pipe seperated list of filters the current location has to satisfy
@@ -8,15 +31,32 @@ export class Url {
      * @returns true if at least on filter is fullfilled
      */
     public static matches(filter: string) {
-        const domain = document.location.protocol + "//" + document.location.host;
+        const instance = this.getInstance();
+        const domain = instance.location.protocol + "//" + instance.location.host;
         let result = false;
         for (const constraint of filter.split("|")) {
             if (constraint.startsWith("=")) {
-                result = result || document.location.href === domain + constraint.substring(1);
+                result = result || instance.location.href === domain + constraint.substring(1);
             } else {
-                result = result || document.location.href.startsWith(domain + constraint);
+                result = result || instance.location.href.startsWith(domain + constraint);
             }
         }
         return result;
+    }
+
+    /**
+     * @return returns the query parameter, or undefined if the key doesn't exist
+     */
+    public static getQueryParameter(key: string) {
+        return this.getInstance().queryParameter.get(key);
+    }
+
+    /**
+     * Returns a singleton instance of the class
+     * @returns Url instance
+     */
+    public static getInstance(): Url {
+        if (this.instance === undefined) this.instance = new Url();
+        return this.instance;
     }
 }
