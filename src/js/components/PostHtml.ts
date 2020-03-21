f2110e936401b48028ea28f94e1f930f06a0df6e
Earlopain <earlopain@c5h8no4na.net>
2020-03-21 19:36:06 +0100

    Added function to create a post dom element from json

---
+++ src/js/components/PostHtml.ts
@@ -0,0 +1,153 @@
+import { ApiPost } from "./apiresponses/ApiPost";
+
+export class PostHtml {
+    public static create(json: ApiPost) {
+        //data-has-sound
+        //data-flags
+        //data-uploader
+        const tags = json.tags;
+        //this must be possible in another way
+        const allTags = tags.artist.concat(tags.character, tags.copyright, tags.general,
+            tags.invalid, tags.lore, tags.meta, tags.species).join(" ");
+        let $article = $("<article>").
+            attr("id", "post_" + json.id).
+            attr("data-id", json.id).
+            attr("data-flags", this.getFlags(json).join(" ")).
+            attr("data-tags", allTags).
+            attr("data-rating", json.rating).
+            attr("data-uploader-id", json.uploader_id).
+            attr("data-file-ext", json.file.ext).
+            attr("data-file-url", json.file.url).
+            attr("data-large-file-url", json.sample.url).       //yes, even though the name has large in it, sample is correct here
+            attr("data-preview-file-url", json.preview.url);
+
+        for (const className of this.getArticleClasses(json)) {
+            $article.addClass(className);
+        }
+        let $href = $("<a>").
+            attr("href", "/posts/" + json.id);
+        let $picture = $("<picture>");
+        $picture.append($("<source>").
+            attr("media", "(max-width: 800px)").
+            attr("srcset", json.preview.url));
+        //TODO title status
+        $picture.append($("<img>").
+            attr("class", "has-cropped-false").
+            attr("src", json.preview.url).
+            attr("title", `Rating: ${json.rating}\nID: ${json.id}\nDate: ${json.created_at}\nScore: ${json.score.total}\n\n ${allTags}`).
+            attr("alt", allTags));
+
+        $href.append($picture);
+        let $desc = $("<div>").
+            attr("class", "desc");
+        let $postScore = $("<div>").attr("id", "post-score-" + json.id).attr("class", "post-score");
+
+        //Score
+        const scoreInfo = this.getScoreInfo(json);
+        $postScore.append($("<span>").
+            addClass("post-score-score ").
+            addClass(scoreInfo.class).
+            text(scoreInfo.modifier + json.score.total));
+        //Favs
+        $postScore.append($("<span>").
+            addClass("post-score-faves").
+            text("\u2665" + json.fav_count));
+        //Comments
+        $postScore.append($("<span>").
+            addClass("post-score-comments").
+            text("C" + json.comment_count));
+        //Rating
+        $postScore.append($("<span>").
+            addClass("post-score-rating").
+            text(json.rating.toUpperCase()));
+        //Extra
+        $postScore.append($("<span>").
+            addClass("post-score-extra").
+            text(this.getExtra(json)));
+
+        //Append to main container
+        $desc.append($postScore);
+        $article.append($href);
+        $article.append($desc);
+        return $article;
+    }
+
+    private static getScoreInfo(json: ApiPost) {
+        if (json.score.total === 0) {
+            return {
+                class: "score-neutral",
+                modifier: "\u2195"
+            };
+        } else if (json.score.total > 0) {
+            return {
+                class: "score-positive",
+                modifier: "\u2191"
+            }
+        } else {
+            return {
+                class: "score-negative",
+                modifier: "\u2193"
+            }
+        }
+    }
+
+    private static getArticleClasses(json: ApiPost) {
+        let result = [
+            "blacklisted",
+            "captioned",
+            "post-preview"
+        ];
+        switch (json.rating) {
+            case "s":
+                result.push("post-rating-safe");
+                break;
+            case "q":
+                result.push("post-rating-questionable");
+                break;
+            case "e":
+                result.push("post-rating-explicit");
+                break;
+        }
+
+        for (const flag of this.getFlags(json)) {
+            result.push("post-status-" + flag);
+        }
+
+        if (json.relationships.has_active_children) {
+            result.push("post-status-has-children");
+        }
+        if (json.relationships.parent_id !== null) {
+            result.push("post-status-has-parent");
+        }
+        return result;
+    }
+
+    private static getFlags(json: ApiPost) {
+        const result = [];
+        if (json.flags.deleted) {
+            result.push("deleted");
+        } else if (json.flags.flagged) {
+            result.push("flagged");
+        } else if (json.flags.pending) {
+            result.push("pending")
+        }
+        return result;
+    }
+
+    private static getExtra(json: ApiPost) {
+        let result = "";
+        if (json.relationships.parent_id !== null) {
+            result += "P";
+        }
+        if (json.relationships.has_active_children) {
+            result += "C";
+        }
+        if (json.flags.pending) {
+            result += "U";
+        }
+        if (json.flags.flagged) {
+            result += "F";
+        }
+        return result;
+    }
+}
---
+++ src/js/components/apiresponses/ApiPost.ts
@@ -0,0 +1,78 @@
+export interface ApiPost {
+    id: number;
+    created_at: string;
+    updated_at: string;
+    file: File;
+    preview: Preview;
+    sample: Sample;
+    score: Score;
+    tags: Tags;
+    locked_tags: string[];
+    change_seq: number;
+    flags: Flags;
+    rating: string;
+    fav_count: number;
+    sources: string[];
+    pools: number[];
+    relationships: Relationships;
+    approver_id?: number;
+    uploader_id: number;
+    description: string;
+    comment_count: number;
+    is_favorited: boolean;
+}
+
+interface Relationships {
+    parent_id?: number;
+    has_children: boolean;
+    has_active_children: boolean;
+    children: number[];
+}
+
+interface Flags {
+    pending: boolean;
+    flagged: boolean;
+    note_locked: boolean;
+    status_locked: boolean;
+    rating_locked: boolean;
+    deleted: boolean;
+}
+
+interface Tags {
+    general: string[];
+    species: string[];
+    character: string[];
+    copyright: string[];
+    artist: string[];
+    invalid: string[];
+    lore: string[];
+    meta: string[];
+}
+
+interface Score {
+    up: number;
+    down: number;
+    total: number;
+}
+
+interface Sample {
+    has: boolean;
+    height: number;
+    width: number;
+    url: string;
+}
+
+interface Preview {
+    width: number;
+    height: number;
+    url: string;
+}
+
+interface File {
+    width: number;
+    height: number;
+    ext: string;
+    size: number;
+    md5: string;
+    url: string;
+}
