bf80326589309427ba1febd7120968fbd0c342ac
Earlopain <earlopain@c5h8no4na.net>
2020-03-22 17:38:32 +0100

    Replace shortended tag count with full one Fixes #39

--- src/js/modules/Miscellaneous.ts
+++ src/js/modules/Miscellaneous.ts
@@ -1,5 +1,7 @@
 import { RE6Module } from "../components/RE6Module";
 import { Url } from "../components/Url";
+import { ApiTag } from "../components/apiresponses/ApiTag";
+import { Api } from "../components/Api";
 
 declare var GM_addStyle;
 declare var GM_getResourceText;
@@ -24,6 +26,11 @@ export class Miscellaneous extends RE6Module {
         // Load the Redesign Fixes stylesheet
         this.loadRedesignFixes(this.fetchSettings("loadRedesignFixes"));
 
+        // Replaces the tag count estimate with the real number
+        if (this.fetchSettings("improveTagCount") === true && Url.matches("/posts")) {
+            this.improveTagCount();
+        }
+
         // Auto-focus on the searchbar
         this.focusSearchBar();
     }
@@ -42,6 +49,36 @@ export class Miscellaneous extends RE6Module {
     private loadRedesignFixes(enabled: boolean = true) {
         this.redesignStylesheet = $(GM_addStyle(GM_getResourceText("redesignFixes")));
         if (!enabled) { this.disableRedesignFixes(); }
+    }
+
+    /**
+     * Replaces the tag estimates with the real count
+     */
+    private async improveTagCount() {
+        const tagElements: Map<string, { tagEscaped: string, element: JQuery<HTMLElement> }> = new Map<string, { tagEscaped: string, element: JQuery<HTMLElement> }>();
+        const $tags = $("#tag-box > ul > li, #tag-list > ul > li");
+        $tags.each(function () {
+            const $this = $(this);
+            //only use the first 320 tags. This should't happen to many posts anyways
+            //only add tags which end in k, meaning they have a ~ count
+            if (tagElements.size !== 320 && $this.find(".post-count").text().endsWith("k")) {
+                //grab the child with the tagname in it, replace spaces with _ and make it url save
+                const tag = $this.find(".search-tag").text();
+                const tagNormal = tag.replace(/ /g, "_");
+                const tagEscaped = encodeURIComponent(tagNormal);
+                tagElements.set(tagNormal, {
+                    tagEscaped: tagEscaped,
+                    element: $this
+                });
+            }
+        });
+        const tagString = Array.from(tagElements.values()).map(e => e.tagEscaped).join(",");
+        const tagJson: ApiTag[] = await Api.getJson("/tags.json?limit=320&search[name]=" + tagString);
+
+        for (const tag of tagJson) {
+            const entry = tagElements.get(tag.name);
+            entry.element.find(".post-count").text(tag.post_count);
+        }
     }
 
     /** Enable the redesign stylesheet */
@@ -69,6 +106,7 @@ export class Miscellaneous extends RE6Module {
         return {
             "removeSearchQueryString": true,
             "loadRedesignFixes": true,
+            "improveTagCount": true
         };
     }
 
