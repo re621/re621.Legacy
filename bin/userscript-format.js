4f1f50917487803f1bf7c935435726c6581686e4
Earlopain <earlopain@c5h8no4na.net>
2020-03-25 20:22:15 +0100

    use npm run build and read tag name from env

--- bin/userscript-format.js
+++ bin/userscript-format.js
@@ -4,16 +4,17 @@ const template = fs.readFileSync("./bin/userscript-template.txt").toString();
 const json = JSON.parse(fs.readFileSync("./package.json"));
 
 var templateReplaced = template.replace(/%NAME%/g, json.displayName).
-replace(/%NAMESPACE%/g, json.namespace).
-replace(/%DESCRIPTION%/g, json.description).
-replace(/%AUTHOR%/g, json.author);
+    replace(/%NAMESPACE%/g, json.namespace).
+    replace(/%DESCRIPTION%/g, json.description).
+    replace(/%AUTHOR%/g, json.author);
 
-if (process.argv[2] === undefined) {
+const tagName = process.env.GIT_TAG_NAME;
+if (tagName === undefined) {
     templateReplaced = templateReplaced.replace(/%VERSION%/g, json.version);
 } else {
     templateReplaced = templateReplaced.replace(/%VERSION%/g,
         json.version.substring(0, json.version.lastIndexOf(".")) +
-        process.argv[2].substring(process.argv[2].lastIndexOf("."))
+        tagName.substring(tagName.lastIndexOf("."))
     );
 }
 
