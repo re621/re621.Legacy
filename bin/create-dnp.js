c5d8970bf9e91290ffa475a86364adb5b21197dc
Earlopain <earlopain@c5h8no4na.net>
2020-03-30 10:37:19 +0200

    Get all dnp entries, not just those on the first page Also remove xmlhttprequest dependency, use native https instead

--- bin/create-dnp.js
+++ bin/create-dnp.js
@@ -1,36 +1,71 @@
 const fs = require("fs");
-const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const https = require('https');
 
-const package = JSON.parse(fs.readFileSync("./package.json"));
-
-loadJSON("https://e621.net/tag_implications.json?search[consequent_name]=avoid_posting",
-    function(data) {
-        let parsedData = {
-            meta: {
-                package: package.name + "/dnp",
-                version: process.env.GIT_TAG_NAME === undefined ? package.version : process.env.GIT_TAG_NAME
-            },
-            data: {}
-        };
-        data.forEach((entry) => {
-            parsedData["data"][entry["antecedent_name"]] = { reason: entry["reason"] }
-        });
-        fs.writeFileSync("./build/avoid-posting.json", JSON.stringify(parsedData, null, 2));
-    },
-    function(xhr) { console.error(xhr); }
-);
-
-function loadJSON(path, success, error) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState !== 4) return;
-        if (xhr.status === 200) {
-            if (success) success(JSON.parse(xhr.responseText));
-        } else {
-            if (error) error(xhr);
-        }
+(async () => {
+    const package = JSON.parse(fs.readFileSync("./package.json"));
+    let parsedData = {
+        meta: {
+            package: package.name + "/dnp",
+            version: process.env.GIT_TAG_NAME === undefined ? package.version : process.env.GIT_TAG_NAME
+        },
+        data: {}
     };
-    xhr.open("GET", path, true);
-    xhr.setRequestHeader("User-Agent", "re621/1.0 buildscript re621.github.io");
-    xhr.send();
+
+    const dnp = await getDnp();
+    dnp.forEach((entry) => {
+        parsedData["data"][entry["antecedent_name"]] = { reason: entry["reason"] }
+    });
+    fs.writeFileSync("./build/avoid-posting.json", JSON.stringify(parsedData, null, 2));
+
+})();
+
+async function getDnp() {
+    let result = [];
+    const url = "https://e621.net/tag_implications.json?search[consequent_name]=avoid_posting&limit=320&page=";
+    let page = 1;
+    let response;
+    do {
+        try {
+            response = await loadJSON(url + page);
+        } catch (error) {
+            console.error(error);
+            process.exit(1);    //exit non-null to abort build process
+        }
+        result = result.concat(response);
+        page++;
+    } while (response.length === 320);
+    return result;
+}
+
+function loadJSON(url) {
+    const options = {
+        hostname: "e621.net",
+        headers: {
+            "User-Agent": "re621/1.0 buildscript re621.github.io"
+        },
+        port: 443,
+        path: url,
+        method: "GET"
+    }
+
+    return new Promise((resolve, reject) => {
+        const request = https.request(options, message => {
+            if (message.statusCode !== 200) {
+                reject();
+            }
+            let result = "";
+            message.on('data', d => {
+                result += d;
+            })
+            message.on("close", () => {
+                resolve(JSON.parse(result));
+            });
+        })
+
+        request.on('error', error => {
+            reject(error);
+        });
+
+        request.end()
+    });
 }
