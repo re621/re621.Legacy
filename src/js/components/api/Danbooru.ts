02e58ff4305b10a0bf27c67fa3570acb426fd988
bitWolfy <bondarchuk.kirill@gmail.com>
2020-04-29 11:05:43 -0700

    Fix unsafeWindow not being detected properly

--- src/js/components/api/Danbooru.ts
+++ src/js/components/api/Danbooru.ts
@@ -2,77 +2,80 @@
 
 import { XM } from "./XM";
 
+declare const unsafeWindow;
+
 export class Danbooru {
 
-    private static modules = XM.Window["Danbooru"];
+    private static getModules(): any { return XM.Window["Danbooru"]; }
+    private static hasModules(): boolean { return XM.Window["Danbooru"] === undefined; }
 
     public static Blacklist: DanbooruBlacklist = {
         apply(): void {
-            if (Danbooru.modules === undefined)
+            if (Danbooru.hasModules())
                 XM.Chrome.execInjectorRequest("Danbooru", "Blacklist", "apply");
-            else Danbooru.modules["Blacklist"].apply();
+            else Danbooru.getModules()["Blacklist"].apply();
         },
 
         initialize_anonymous_blacklist(): void {
-            if (Danbooru.modules === undefined)
+            if (Danbooru.hasModules())
                 XM.Chrome.execInjectorRequest("Danbooru", "Blacklist", "initialize_anonymous_blacklist");
-            else Danbooru.modules["Blacklist"].initialize_anonymous_blacklist();
+            else Danbooru.getModules()["Blacklist"].initialize_anonymous_blacklist();
         },
 
         initialize_all(): void {
-            if (Danbooru.modules === undefined)
+            if (Danbooru.hasModules())
                 XM.Chrome.execInjectorRequest("Danbooru", "Blacklist", "initialize_all");
-            else Danbooru.modules["Blacklist"].initialize_all();
+            else Danbooru.getModules()["Blacklist"].initialize_all();
         },
 
         initialize_disable_all_blacklists(): void {
-            if (Danbooru.modules === undefined)
+            if (Danbooru.hasModules())
                 XM.Chrome.execInjectorRequest("Danbooru", "Blacklist", "initialize_disable_all_blacklists");
-            else Danbooru.modules["Blacklist"].initialize_disable_all_blacklists();
+            else Danbooru.getModules()["Blacklist"].initialize_disable_all_blacklists();
         },
 
         stub_vanilla_functions(): void {
-            if (Danbooru.modules === undefined)
+            if (Danbooru.hasModules())
                 XM.Chrome.execInjectorRequest("Danbooru", "Blacklist", "stub_vanilla_functions");
             else {
-                Danbooru.modules["Blacklist"].apply = (): void => { return; };
-                Danbooru.modules["Blacklist"].initialize_disable_all_blacklists = (): void => { return; };
-                Danbooru.modules["Blacklist"].initialize_all = (): void => { return; };
+                Danbooru.getModules()["Blacklist"].apply = (): void => { return; };
+                Danbooru.getModules()["Blacklist"].initialize_disable_all_blacklists = (): void => { return; };
+                Danbooru.getModules()["Blacklist"].initialize_all = (): void => { return; };
             }
         },
     }
 
     public static Post: DanbooruPost = {
         vote(postid: number, scoreDifference: number, preventUnvote?: boolean): void {
-            if (Danbooru.modules === undefined)
+            if (Danbooru.hasModules())
                 XM.Chrome.execInjectorRequest("Danbooru", "Post", "vote", [postid, scoreDifference, preventUnvote]);
-            else Danbooru.modules["Post"].vote(postid, scoreDifference, preventUnvote);
+            else Danbooru.getModules()["Post"].vote(postid, scoreDifference, preventUnvote);
         }
     };
 
     public static Note: DanbooruNote = {
         Box: {
             scale_all(): void {
-                if (Danbooru.modules === undefined)
+                if (Danbooru.hasModules())
                     XM.Chrome.execInjectorRequest("Danbooru", "Note.Box", "scale_all");
-                else Danbooru.modules["Note"]["Box"].scale_all();
+                else Danbooru.getModules()["Note"]["Box"].scale_all();
             }
         },
 
         TranslationMode: {
             active(state?: boolean): Promise<boolean> {
-                if (Danbooru.modules === undefined)
+                if (Danbooru.hasModules())
                     return XM.Chrome.execInjectorRequest("Danbooru", "Note.TranslationMode", "active", [state]);
                 else {
-                    if (state !== undefined) Danbooru.modules["Note"]["TranslationMode"].active = state;
-                    return Promise.resolve(Danbooru.modules["Note"]["TranslationMode"].active);
+                    if (state !== undefined) Danbooru.getModules()["Note"]["TranslationMode"].active = state;
+                    return Promise.resolve(Danbooru.getModules()["Note"]["TranslationMode"].active);
                 }
             },
 
             toggle(): void {
-                if (Danbooru.modules === undefined)
+                if (Danbooru.hasModules())
                     XM.Chrome.execInjectorRequest("Danbooru", "Note.TranslationMode", "toggle");
-                else Danbooru.modules["Note"]["TranslationMode"].toggle(new CustomEvent("re621.dummy-event"));
+                else Danbooru.getModules()["Note"]["TranslationMode"].toggle(new CustomEvent("re621.dummy-event"));
             },
         }
     };
@@ -80,26 +83,26 @@ export class Danbooru {
     public static Utility: DanbooruUtility = {
 
         disableShortcuts(state?: boolean): Promise<boolean> {
-            if (Danbooru.modules === undefined)
+            if (Danbooru.hasModules())
                 return XM.Chrome.execInjectorRequest("Danbooru", "Utility", "disableShortcuts", [state]);
             else {
-                if (state !== undefined) Danbooru.modules["Utility"].disableShortcuts = state;
-                return Promise.resolve(Danbooru.modules["Utility"].disableShortcuts);
+                if (state !== undefined) Danbooru.getModules()["Utility"].disableShortcuts = state;
+                return Promise.resolve(Danbooru.getModules()["Utility"].disableShortcuts);
             }
         },
 
     };
 
     public static notice(input: string): void {
-        if (Danbooru.modules === undefined)
+        if (Danbooru.hasModules())
             XM.Chrome.execInjectorRequest("Danbooru", "Notice", "notice", [input]);
-        else Danbooru.modules.notice(input);
+        else Danbooru.getModules()["notice"](input);
     }
 
     public static error(input: string): void {
-        if (Danbooru.modules === undefined)
+        if (Danbooru.hasModules())
             XM.Chrome.execInjectorRequest("Danbooru", "Notice", "error", [input]);
-        else Danbooru.modules.error(input);
+        else Danbooru.getModules()["error"](input);
     }
 }
 
