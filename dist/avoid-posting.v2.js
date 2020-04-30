2863b119b1f7df0d761ffb528428030536b31722
bitWolfy <bondarchuk.kirill@gmail.com>
2020-04-30 13:08:22 -0700

    Add a test version of DNPv2

---
+++ dist/avoid-posting.v2.js
@@ -0,0 +1,11 @@
+fetchDNP({
+    "meta": {
+        "package": "re621/dnp",
+        "version": "200430:1306"
+    },
+    "data": [
+        "7mii",
+        "abobarseem",
+        "accell"
+    ]
+});
