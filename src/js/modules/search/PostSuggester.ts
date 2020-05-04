1ee8c42f8d70a482d9bc84f091a65aa7aa9f702b
bitWolfy <bondarchuk.kirill@gmail.com>
2020-05-04 13:17:20 -0700

    Resolve PostSuggester request issues

--- src/js/modules/search/PostSuggester.ts
+++ src/js/modules/search/PostSuggester.ts
@@ -17,19 +17,19 @@ export class PostSuggester extends RE6Module {
         "hi_res", "absurd_res", "digital_media_(artwork)", // meta
         "solo", "duo", "group", // composition
         "simple_background", "detailed_background", // background
-        "text", // misc
+        "text", "english_text", // misc
     ];
 
     /** These tags are displayed, by not selected */
     private static ignoredTags = [
-        "male", "female", // genders
-        "mammal", "scalie", // species
+        "male", "female", "intersex", // genders
+        "mammal", "scalie", "biped", // species
         "anthro", "feral", // species type
-        "genitals", "penis", "balls", "animal_genitalia", "humanoid_penis", "erection", "pussy", "butt", "anus", "breasts", "nipples", "non-mammalian_breasts", // genitalia
+        "genitals", "penis", "balls", "animal_genitalia", "humanoid_penis", "erection", "pussy", "butt", "anus", "breasts", "nipples", "non-mammal_breasts", // genitalia
         "genital_fluids", "bodily_fluids", "cum", // fluids
-        "wings", "membrane_(anatomy)", "claws", "horn", "scales", "teeth", "smile", "tongue", "feathers", // body parts
+        "wings", "membrane_(anatomy)", "membranous_wings", "claws", "horn", "scales", "teeth", "smile", "tongue", "feathers", "toes", // body parts
         "hair", "fur", "clothing", "clothed", "nude", // misc
-        "sex", "penetration", // actions
+        "sex", "penetration", "male_penetrating", // actions
     ];
 
     private status: JQuery<HTMLElement>;
@@ -52,11 +52,6 @@ export class PostSuggester extends RE6Module {
         const listItem = $("<li>").appendTo("ul#related-list");
         const button = $("<a>")
             .html("Recommended")
-            .click((event) => {
-                event.preventDefault();
-                this.handleRecommendation();
-                // TODO Block repeat clicks while the queue is processing
-            })
             .appendTo(listItem);
 
         // Create modal content
@@ -92,20 +87,20 @@ export class PostSuggester extends RE6Module {
     public async handleRecommendation(): Promise<boolean> {
         this.status.html("Compiling favorites data");
         const data: TagCounter = {};
-        let result: APIPost[],
-            index = 1;
-        do {
+        let result: APIPost[];
+        for (let i = 1; i <= PostSuggester.maxPages; i++) {
             // Fetching data from the API
-            this.status.html(`Analyzing favorites [ ${index} / ${PostSuggester.maxPages} ]`);
-            result = await E621.Posts.get<APIPost>({ tags: "fav:bitWolfy", page: index, limit: 320 }, 500);
+            this.status.html(`Analyzing favorites [ ${i} / ${PostSuggester.maxPages} ]`);
+            result = await E621.Posts.get<APIPost>({ tags: "fav:Lunacy", page: i, limit: 320 }, 500); // TODO Use the actual username
             result.forEach((post) => {
                 APIPost.getTags(post).forEach((tag) => {
                     if (data[tag]) data[tag] = data[tag] + 1;
                     else data[tag] = 1;
                 });
             });
-            index++;
-        } while (result.length == 320 && index <= PostSuggester.maxPages);
+
+            if (result.length !== 320) break;
+        }
 
         this.status.html("Lookup complete");
 
@@ -114,11 +109,16 @@ export class PostSuggester extends RE6Module {
         for (const tag in data) processedData.push([tag, data[tag]]);
         processedData = processedData.sort((a, b) => { return b[1] - a[1]; });
 
+        // Add year tags
+        for (let year = 1973; year <= new Date().getFullYear(); year++)
+            PostSuggester.removedTags.push(year + "");
+
         // Filter
         processedData = processedData.filter((entry) => {
             return !PostSuggester.removedTags.includes(entry[0]) &&
                 entry[1] > PostSuggester.minTagCount;
         });
+        if (processedData.length > 100) processedData.length = 100;
 
         // Output
         this.content[0].innerHTML = "";
