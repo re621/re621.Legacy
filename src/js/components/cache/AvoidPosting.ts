a2da714497c9eb517426a4eb18581e42f3c0fe11
bitWolfy <bondarchuk.kirill@gmail.com>
2020-07-31 19:25:15 -0700

    Cache classes documentation

--- src/js/components/cache/AvoidPosting.ts
+++ src/js/components/cache/AvoidPosting.ts
@@ -6,34 +6,47 @@ export class AvoidPosting {
 
     private static cache: Set<string>;
 
+    /**
+     * Returns the set containing cached items.  
+     * Loads the data from local storage if necessary.
+     */
     private static getCache(): Set<string> {
         if (typeof AvoidPosting.cache === "undefined")
             AvoidPosting.cache = new Set<string>(JSON.parse(window.localStorage.getItem("re621.dnpcache.data") || "[]"))
         return AvoidPosting.cache;
     }
 
+    /** Saves the cached items to local storage */
     private static save(): void {
         window.localStorage.setItem("re621.dnpcache.data", JSON.stringify(Array.from(AvoidPosting.getCache())));
     }
 
+    /** Permanently removes all data from cache */
     private static clear(): void {
         AvoidPosting.cache = new Set();
         AvoidPosting.save();
     }
 
+    /** Returns the number of items in the cache */
     public static size(): number {
         return AvoidPosting.getCache().size;
     }
 
+    /** Returns true if the parameter is present in cache */
     public static has(tag: string): boolean {
         return AvoidPosting.getCache().has(tag);
     }
 
+    /** Adds the provided item to cache */
     private static add(tag: string): void {
         AvoidPosting.getCache().add(tag);
         AvoidPosting.save();
     }
 
+    /**
+     * Fetches the tags aliased to `avoid_posting` from the API and adds them to cache
+     * @param status DOM element for the status messages
+     */
     public static async update(status?: JQuery<HTMLElement>): Promise<number> {
         if (!status) status = $("<span>");
 
@@ -43,7 +56,7 @@ export class AvoidPosting {
 
         do {
             page++;
-            status.html(`<i class="fas fa-circle-notch fa-spin"></i> Processing favorites: batch ${page} / ?`)
+            status.html(`<i class="fas fa-circle-notch fa-spin"></i> Processing tags: batch ${page} / ?`)
             result = await E621.TagImplications.get<APITagImplication>({ "search[consequent_name]": "avoid_posting", page: page, limit: 320 }, 500);
             for (const entry of result) AvoidPosting.add(entry["antecedent_name"]);
         } while (result.length == 320);
@@ -55,10 +68,12 @@ export class AvoidPosting {
         return Promise.resolve(AvoidPosting.size());
     }
 
+    /** Returns the timestamp for the last time cache was updated */
     public static getUpdateTime(): number {
         return parseInt(window.localStorage.getItem("re621.dnpcache.update")) || 0;
     }
 
+    /** Returns true if the cache needs to be reloaded */
     public static isUpdateRequired(): boolean {
         return (AvoidPosting.getUpdateTime() + Util.Time.DAY < Util.Time.now()) || AvoidPosting.size() == 0;
     }
--- src/js/components/cache/FavoriteCache.ts
+++ src/js/components/cache/FavoriteCache.ts
@@ -7,34 +7,48 @@ export class FavoriteCache {
 
     private static cache: Set<number>;
 
+    /**
+     * Returns the set containing cached items.  
+     * Loads the data from local storage if necessary.
+     */
     private static getCache(): Set<number> {
         if (typeof FavoriteCache.cache === "undefined")
             FavoriteCache.cache = new Set<number>(JSON.parse(window.localStorage.getItem("re621.favcache.data") || "[]"))
         return FavoriteCache.cache;
     }
 
+    /** Saves the cached items to local storage */
     private static save(): void {
         window.localStorage.setItem("re621.favcache.data", JSON.stringify(Array.from(FavoriteCache.getCache())));
     }
 
+    /** Permanently removes all data from cache */
     private static clear(): void {
         FavoriteCache.cache = new Set();
         FavoriteCache.save();
     }
 
+    /** Returns the number of items in the cache */
     public static size(): number {
         return FavoriteCache.getCache().size;
     }
 
+    /** Returns true if the parameter is present in cache */
     public static has(post: number): boolean {
         return FavoriteCache.getCache().has(post);
     }
 
+    /** Adds the provided item to cache */
     public static add(post: number): void {
         FavoriteCache.getCache().add(post);
         FavoriteCache.save();
     }
 
+    /**
+     * Removes the provided item to cache
+     * @param post Item to remove
+     * @returns true if the item was present in the cache, false otherwise
+     */
     public static remove(post: number): boolean {
         if (!FavoriteCache.has(post)) return false;
         FavoriteCache.getCache().delete(post);
@@ -42,6 +56,10 @@ export class FavoriteCache {
         return true;
     }
 
+    /**
+     * Iterates over the current user's favorites and adds their IDs to cache
+     * @param status DOM element for the status messages
+     */
     public static async update(status?: JQuery<HTMLElement>): Promise<number> {
         if (!status) status = $("<span>");
 
@@ -65,10 +83,12 @@ export class FavoriteCache {
         return Promise.resolve(FavoriteCache.size());
     }
 
+    /** Returns the timestamp for the last time cache state was verified */
     public static getUpdateTime(): number {
         return parseInt(window.localStorage.getItem("re621.favcache.update")) || 0;
     }
 
+    /** Returns true if the cache needs to be reloaded */
     public static async isUpdateRequired(): Promise<boolean> {
         if (FavoriteCache.getUpdateTime() + Util.Time.DAY < Util.Time.now()) {
             const updateRequired = (await User.getCurrentSettings()).favorite_count !== FavoriteCache.size();
