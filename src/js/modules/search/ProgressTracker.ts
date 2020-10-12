5c321ef9c68393ab2ac6b920f0e19e7d0f9ad250
bitWolfy <bondarchuk.kirill@gmail.com>
2020-10-12 13:23:22 -0700

    Clean up the ProgressTracker

--- src/js/modules/search/ProgressTracker.ts
+++ src/js/modules/search/ProgressTracker.ts
@@ -19,22 +19,33 @@ export class ProgressTracker extends RE6Module {
     public create(): void {
         super.create();
 
-        let data = this.fetchSettings<TrackedData>("data");
-        const tags = Page.getQueryParameter("tags") || "";
+        const data = this.fetchSettings<TrackedData>("data");
+        const tags = (Page.getQueryParameter("tags") || "").trim().toLowerCase();
 
         // Don't track the main search page
         if (tags == "") return;
+
+        // Build the structure once BetterSearch has loaded
+        BetterSearch.one("ready", () => {
+            this.createDOM(data, tags);
+        });
+
+    }
+
+    private createDOM(data: TrackedData, tags: string): void {
+
         const lastPost = data[tags];
+        const page = Page.getQueryParameter("page") || "1";
 
-        const container = $("<search-track>")
-            .prependTo("search-content");
+        const container = $("<span>")
+            .prependTo("search-stats");
 
+        // Create the structure
         const trackButton = $("<a>")
-            .on("click", () => {
-                data = this.fetchSettings<TrackedData>("data");
-                const post = Post.get("first");
+            .on("click", async () => {
+                data = await this.fetchSettings<TrackedData>("data", true);
                 if (data[tags] == undefined) {
-                    data[tags] = post == null ? -1 : post.id;
+                    data[tags] = getNewPost();
                     this.pushSettings("data", data);
                     trackButton.html("Untrack");
                 } else {
@@ -43,25 +54,32 @@ export class ProgressTracker extends RE6Module {
                     trackButton.html("Track");
                 }
             })
-            .html(data[tags] == undefined ? "Track" : "Untrack")
-            .appendTo(container);
+            .html(lastPost == undefined ? "Track" : "Untrack")
+            .attr("title", "Highlight previously unseen posts")
+            .prependTo(container);
 
-        // Add a "new" attribute to unseen posts
+        // Abort if the page is not being tracked
         if (lastPost == undefined) return;
 
-        BetterSearch.on("ready", () => {
-            const post = Post.get("first");
-            data[tags] = post == null ? -1 : post.id;
-            this.pushSettings("data", data);
+        // Avoid issues when opening posts on other pages
+        if (page != "1") return;
 
-            if (lastPost == -1) return;
-            Post.find(lastPost).each((post) => {
-                post.$ref.attr({
-                    "new": true,
-                });
-            });
+        data[tags] = getNewPost();
+        this.pushSettings("data", data);
+
+        // Add "new" attribute to unseen posts
+        if (lastPost == -1) return;
+        Post.find(lastPost).each((post) => {
+            post.$ref.attr("new", "true");
         });
 
+        function getNewPost(): number {
+            const post = Post.get("first");
+            const page = Page.getQueryParameter("page") || "1";
+            if (post == null) return -1;     // Must have some posts on the page
+            if (page !== "1") return -1;     // Only record the posts on the first page
+            return post.id;
+        }
     }
 
 }
