47d7e30a0e8b6cbd59e9b2ba250eb210785c5754
bitWolfy <bondarchuk.kirill@gmail.com>
2021-03-13 23:57:07 -0800

    Disable updates on other trackers for now

--- src/js/modules/subscriptions/ForumTracker.ts
+++ src/js/modules/subscriptions/ForumTracker.ts
@@ -1,7 +1,21 @@
+import { Settings } from "../../components/RE6Module";
 import { SubscriptionTracker } from "./_SubscriptionTracker";
 
 export class ForumTracker extends SubscriptionTracker {
 
+    public getDefaultSettings(): Settings {
+        return {
+            enabled: true,
 
+            // User-customizable settings
+            updateInterval: -1,
+            cacheMaxAge: 0,                 // cache entries older than this get trimmed
+            cacheSize: 60,                  // how many subscription updates are kept
+
+            // Utility values
+            lastUpdate: 0,                  // last time an update has been completed
+            lastAttempt: 0,                 // last time an update was attempted. 0 if not applicable.
+        }
+    }
 
 }
