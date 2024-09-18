019c41578f2e128d5a7870f6fcdc8510ce90cfdf
hackerTiger21 <36011747+hackerTiger21@users.noreply.github.com>
2024-09-17 21:26:46 -0600

    Alternate approach to objective
    Previous method didn't work with triggering focus at first.

--- src/js/modules/search/SearchUtilities.ts
+++ src/js/modules/search/SearchUtilities.ts
@@ -199,13 +199,13 @@ export class SearchUtilities extends RE6Module {
     private focusSearchbar(event: Event): void {
         event.preventDefault();
         const searchbar = $("section#search-box input");
-        searchbar.trigger("focus");
         if(this.fetchSettings("switchCursorFocus")){
             searchbar.each(function () {
                 const textbox = this as HTMLInputElement;
+                textbox.focus();
                 textbox.setSelectionRange(textbox.value.length, textbox.value.length);
             });
-        };
+        } else searchbar.trigger("focus");;
     }
 
     /** Switches the location over to a random post */
